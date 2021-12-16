c98e855494ccbef081555b15ab53291452dcb7b4
Nikola Jovanovic <jovanovicn369@gmail.com>
2021-12-16 14:28:44 +0100

    Private and public routes, teacherService

---
+++ src/services/TeacherService.js
@@ -0,0 +1,34 @@
+import HttpService from "./HttpService";
+
+
+class TeacherService extends HttpService {
+    
+    getAll = async () => {
+        try {
+            const { data } = await this.client.get("teachers");
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+
+        return [];
+    }
+    getAvailable = async () => {
+        try {
+            const { data } = await this.client.get("teachers/available");
+            return data;
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+        return [];
+    }
+
+    
+    
+}
+
+const teacherService = new TeacherService();
+export default teacherService;
+
---
+++ src/store/teachers/selectors.js
@@ -0,0 +1,4 @@
+export function selectTeachers(state){
+    return state.teachers.data;
+}
+
